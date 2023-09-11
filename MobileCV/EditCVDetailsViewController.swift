769a711320c45f361e44856ca5bc7b52fe9c3e26
tesddev <tesleem.amuda@gmail.com>
2023-09-11 10:36:29 +0100

    add new vc

---
+++ MobileCV/EditCVDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EditCVDetailsViewController.swift
+//  MobileCV
+//
+//  Created by GIGL iOS on 11/09/2023.
+//
+
+import UIKit
+
+class EditCVDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
