976154a5d425b1907d713ce4210b5328d2db3eb1
tesddev <tesleem.amuda@gmail.com>
2023-09-11 09:36:47 +0100

    Initial Commit

---
+++ MobileCV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileCV
+//
+//  Created by GIGL iOS on 11/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
