2715b8f7f96fd332026ab4b204798ada246c095d
tesddev <tesleem.amuda@gmail.com>
2023-09-11 10:36:00 +0100

    add cv model

---
+++ MobileCV/CVModel.swift
@@ -0,0 +1,16 @@
+//
+//  CVModel.swift
+//  MobileCV
+//
+//  Created by Tes on 11/09/2023.
+//
+
+import Foundation
+
+struct CVModel {
+    /// Pickup details VC
+    static var fullName: String = "Tesleem Amuda"
+    static var slackName: String = "Tes"
+    static var githubHandle: String = "tedddev"
+    static var bio: String = "Experienced iOS developer with a strong background in Swift and other iOS frameworks. \n\nProven track record of developing high-quality, user-friendly mobile apps that have been featured in the App Store. \n\nSkilled in collaborating with cross-functional teams to deliver innovative solutions on time and within budget. \n\nAlso skilled in technologies like Swift Programming Language, Version Control, Cocoa Touch, Jira, Trello, Azure, among others, integrating 3rd party libraries, consuming Restful APIs, working with Agile/Scrum team in Sprints. \n\nCommitted to staying current with the latest technologies and industry trends. Obsessed with detail and quality."
+}
